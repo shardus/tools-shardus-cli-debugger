fc7a812f35e94685ce04771c971b5d2c3bf09d44
asyed94 <asyed94@gmail.com>
2019-10-16 19:03:26 -0500

    Adds support for case when remote server is running local instances.

--- src/lib/collect.js
+++ src/lib/collect.js
@@ -12,6 +12,10 @@ async function collect (instanceUrl, networkDir, progressFn) {
 
   // Stream and extract tar.gz debug file into each instances dir
   for (const [index, node] of nodelist.entries()) {
+    // Hacks for the case when a remote server is running local instances
+    if (instanceUrl.split(':').length === 2) instanceUrl = instanceUrl.split(':')[0]
+    if (node.externalIp === '127.0.0.1' && instanceUrl !== '127.0.0.1') node.externalIp = instanceUrl
+
     const debugUrl = new URL('/debug', `http://${node.externalIp}:${node.externalPort}`)
     const savePath = path.join(networkDir, `debug-${debugUrl.hostname}-${debugUrl.port}`)
     await ensureExists(savePath)
